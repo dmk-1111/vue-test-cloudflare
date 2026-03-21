eca0968fff74616989d35884be20f21fbabf824c
mr-dark <motedee02@gmail.com>
2026-03-21 22:07:29 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const name = ref('');
+
+onMounted(() => {
+  fetch('api/message')
+    .then((resp) => resp.text())
+    .then((text) => {
+      name.value = text;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>{{ name }}</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation

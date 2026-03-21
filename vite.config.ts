52e4c4ca6f556ef3ce3f7e889e7f0d76b3948004
mr-dark <motedee02@gmail.com>
2026-03-21 22:15:09 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8787',
+        target: 'https://my-api.motedee02.workers.dev',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
